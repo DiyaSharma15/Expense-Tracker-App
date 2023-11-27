f0419d861c675c669eae901bf19324164499e5bc
DiyaSharma15 <harry.diya161@gmail.com>
2023-11-27 10:54:38 -0700

    initial commit

---
+++ components/header.js
@@ -0,0 +1,28 @@
+import React from "react"; 
+import { StyleSheet, Text, View } from 'react-native';
+
+export default function Header({ heading }){
+
+    return(
+        <View style={styles.header}>
+            <Text style={styles.headingText}> { heading } </Text>
+        </View>
+        
+    );
+}
+
+const styles = StyleSheet.create({
+    header: {
+      flex: 1,
+      backgroundColor: '#fff',
+      alignItems: 'center',
+      justifyContent: 'flex-start',
+      marginTop: 30,
+    },
+    headingText: {
+        fontSize: 40,
+        backgroundColor: '#FFEACA',
+        borderRadius: 15,
+        padding: 20,
+    }
+  });
