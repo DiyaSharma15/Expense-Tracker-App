587efdda9de7ba21ac03797672bd9b230978aa69
Brar Sarb <39264573+sarbjee@users.noreply.github.com>
2023-12-10 17:46:17 -0700

    Update AddExpense.js

--- components/AddExpense.js
+++ components/AddExpense.js
@@ -18,19 +18,28 @@ export default function ToDoForm({ addExpense }) {
 }
 
 const styles = StyleSheet.create({
-    form: {
-      flexDirection: 'row',
-      justifyContent: 'space-between',
-      alignItems: 'center',
-      marginHorizontal: 20,
-      marginTop: 20,
-    },
-    input: {
-      flex: 1,
-      borderWidth: 1,
-      borderColor: '#ccc',
-      paddingHorizontal: 10,
-      paddingVertical: 8,
-      marginRight: 10,
-    },
-  });
+  form: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+    alignItems: 'center',
+    marginHorizontal: 20,
+    marginTop: 20,
+    backgroundColor: '#fff',
+    padding: 10,
+    borderRadius: 8,
+    shadowColor: '#000',
+    shadowOffset: { width: 0, height: 2 },
+    shadowOpacity: 0.2,
+    shadowRadius: 2,
+    elevation: 3,
+  },
+  input: {
+    flex: 1,
+    borderWidth: 1,
+    borderColor: '#ccc',
+    paddingHorizontal: 10,
+    paddingVertical: 8,
+    marginRight: 10,
+    borderRadius: 4,
+  },
+});
