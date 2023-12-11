765b42e91e84f2f19f5d6e12ebf31f036d1e10fa
DiyaSharma15 <harry.diya161@gmail.com>
2023-12-10 17:30:58 -0700

    AddExpensesScreen modified along with SetLimitsScreen

--- navigaton/screens/AddExpensesScreen.js
+++ navigaton/screens/AddExpensesScreen.js
@@ -1,21 +1,31 @@
 import React, { useState } from 'react';
-import { View, Text, StyleSheet } from 'react-native';
+import { View, Text, StyleSheet, Button } from 'react-native';
 import Header from '../../components/header';
 import Expenses from '../../components/Expenses';
 import ToDoForm from '../../components/AddExpense';
 
-export default function AddExpensesScreen() {
-    const [expenses, setExpenses] = useState([' ']);
-     
+export default function AddExpensesScreen({ navigation }) {
+    const [expenses, setExpenses] = useState(['Rent']);
+
     const addExpense = (expense) => {
-        setExpenses([...expenses, expense])
+        setExpenses([...expenses, expense]);
     };
-     
+
+    const goToHome = () => {
+        navigation.navigate('Home', { expenses });
+    };
+
+    const goToSetLimits = () => {
+        navigation.navigate('Set Limits', { expenses });
+    };
+
     return (
         <View style={styles.container}>
             <Header heading={'Add Expenses'}/>
             <Expenses expense={expenses} />
             <ToDoForm addExpense={addExpense} />
+            <Button title="View Expenses" onPress={goToHome} />
+            <Button title="Go to Set Limits" onPress={goToSetLimits} />
         </View>
     );
 }
--- navigaton/screens/HomeScreen.js
+++ navigaton/screens/HomeScreen.js
@@ -2,10 +2,18 @@ import * as React from 'react';
 import { View, Text, StyleSheet } from 'react-native';
 import Header from '../../components/header';
 
-export default function HomeScreen() {
+export default function HomeScreen({ route }) {
+    const { expenses } = route.params || { expenses: [] };
+
     return (
         <View style={styles.container}>
             <Header heading={'Expense Minder'}/>
+            <Text style={styles.text}>Expenses:</Text>
+            <View>
+                {expenses.map((expense, index) => (
+                    <Text key={index}>{expense}</Text>
+                ))}
+            </View>
         </View>
     );
 }
